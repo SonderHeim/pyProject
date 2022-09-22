0456139f6e2e076ea0759a325126ec01be442dd9
Reinhard <deusexmachina1973@ya.ru>
2022-09-22 13:41:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/BearsHome")
+def bearsHome():
+    return render_template('BearsHome.html')
+
+@app.route("/Home")
+def home():
+    return render_template('Home.html')
+
+@app.route("/aboutTeam")
+def aboutTeam():
+    return render_template('aboutTeam.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
